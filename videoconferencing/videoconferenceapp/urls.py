d160e1e9f966df49022577aa2ad7196a9c6e91bb
Lothar Tjipueja <111390558+lothartj@users.noreply.github.com>
2023-08-14 16:03:01 +0200

    Add files via upload

---
+++ videoconferencing/videoconferenceapp/urls.py
@@ -0,0 +1,12 @@
+from . import views
+from django.urls import path, include
+
+urlpatterns = [
+    path('',views.register, name='register'),
+    path('login/',views.login_view, name='login'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('meeting/',views.videocall, name='meeting'),
+    path('logout/',views.logout_view, name='logout'),
+    path('join/',views.join_room, name='join_room'),
+    path('',views.index, name='index'),
+]
